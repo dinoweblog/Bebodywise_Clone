eb5f747397d3c456d9eccd62ad64f40458c8062f
Dinesh Sharma <93374756+dinoweblog@users.noreply.github.com>
2022-02-15 17:03:42 +0530

    make it better

--- src/server.js
+++ src/server.js
@@ -1,11 +1,11 @@
 const app = require("./index");
 const connect = require("./configs/db");
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
-app.listen(port, async () => {
+app.listen(port, "0.0.0.0", async () => {
   try {
     await connect();
-    console.log(`listening on port 3000${port}`);
+    console.log(`listening on port ${port}`);
   } catch (e) {
     console.log(e.message);
   }
