f53ac1792b32499bc26140f3b2c59309b54cc1ee
Dinesh Sharma <93374756+dinoweblog@users.noreply.github.com>
2022-02-15 17:04:22 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const app = require("./index");
 const connect = require("./configs/db");
 const port = process.env.PORT || 5000;
 
-app.listen(port, async () => {
+app.listen(port, "0.0.0.0", async () => {
   try {
     await connect();
     console.log(`listening on port ${port}`);
