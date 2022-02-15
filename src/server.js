9f5e999e876fbe9e774020673c82b3080d653063
Dinesh Sharma <93374756+dinoweblog@users.noreply.github.com>
2022-02-15 16:51:33 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const app = require("./index");
 const connect = require("./configs/db");
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, async () => {
   try {
